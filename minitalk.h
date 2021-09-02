5720c6306182d9b2a48b8a84a80152cdc0f1f99e
jhonatan229 <jhonatan229@outlook.com>
2021-09-02 18:38:12 -0300

    fix norm

--- minitalk.h
+++ minitalk.h
@@ -6,11 +6,11 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 18:25:27 by jestevam          #+#    #+#             */
-/*   Updated: 2021/09/02 18:37:38 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/09/02 18:38:07 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef MINITALK_H
+#ifndef MINITALK_H
 # define MINITALK_H
 
 # include <signal.h>
@@ -22,4 +22,4 @@ size_t	ft_strlen(char *s);
 int		ft_atoi(const char *nptr);
 void	ft_putnbr_fd(int nb, int fd);
 
-# endif
+#endif
